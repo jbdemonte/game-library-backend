f1c19071e61c8ab85d4c6b8de8e426e18a5eb11e
Jean-Baptiste DEMONTE <jbdemonte@gmail.com>
2021-09-05 21:03:57 +0200

    compress not compressed files

--- src/dropzone/index.ts
+++ src/dropzone/index.ts
@@ -1,13 +1,15 @@
 import fs from 'fs';
 import { basename, join } from 'path';
+import yazl from 'yazl';
 import { FileWatcher } from '../tools/file-watcher';
 import { isZip } from '../tools/zip';
 import { IFileHash } from './lib/file-hash.interface';
 import { getHashFileFromFile, getHashFileFromZipFile } from './lib/get-hash';
 import { getRomSystemId } from './lib/rom-system';
-import { getFileSize } from '../tools/file';
+import { getFileSize, replaceExtension } from '../tools/file';
 import { romModel } from '../models/rom.model';
 import { findRom } from './lib/find-rom';
+
 
 export async function startDropZoneScan() {
   const watcher = new FileWatcher(process.env.DROPZONE_PATH || '', onFile);
@@ -16,19 +18,16 @@ export async function startDropZoneScan() {
 
 async function onFile(path: string) {
   let fileHashes: IFileHash;
-  let size: number;
   if (isZip(path)) {
     fileHashes = await getHashFileFromZipFile(path);
-    size = await getFileSize(path);
   } else {
     fileHashes = await getHashFileFromFile(path);
-    size = fileHashes.files[0].size;
   }
-  await handleFile(fileHashes, size);
+  await handleFile(fileHashes);
 }
 
 
-async function handleFile(fileHashes: IFileHash, size: number) {
+async function handleFile(fileHashes: IFileHash) {
   let rom = await findRom(fileHashes);
   if (rom) {
     console.log(`already existing, deleting ${basename(fileHashes.path)}`);
@@ -39,18 +38,47 @@ async function handleFile(fileHashes: IFileHash, size: number) {
       throw new Error(`System not found for file ${fileHashes.path}`);
     }
     console.log(`new rom for ${system} : ${basename(fileHashes.path)}`);
-    rom = new romModel({...fileHashes, size, system});
-    rom.path = await moveFile(fileHashes.path, process.env.ROMS_PATH || '', system, rom.id);
+    rom = new romModel({...fileHashes, system});
+    rom.path = await saveRomFile(fileHashes.path, process.env.ROMS_PATH || '', system, rom.id);
+    rom.size = await getFileSize(join(process.env.ROMS_PATH || '', rom.path));
     await rom.save();
   }
 }
 
-async function moveFile(source: string, targetPath: string, system: string, id: string): Promise<string> {
+function compressFile(sourceFile: string, targetFile: string): Promise<string> {
+  const zipFile = new yazl.ZipFile();
+
+  const fileName = basename(sourceFile);
+
+  zipFile.addFile(sourceFile, fileName);
+
+  // replace extension by .zip
+  const zipName = fileName.replace(/\.[^.]+$/, '.zip');
+
+  zipFile.end();
+
+  return new Promise((resolve, reject) => {
+    zipFile
+      .outputStream
+      .pipe(fs.createWriteStream(targetFile))
+      .on('close', () => resolve(zipName))
+      .on('error', reject);
+  });
+}
+
+async function saveRomFile(source: string, targetPath: string, system: string, id: string): Promise<string> {
   if (!targetPath) {
     throw new Error('target path is missing');
   }
   await fs.promises.mkdir(join(targetPath, system, id), { recursive: true });
-  const relativePath = join(system, id, basename(source));
-  await fs.promises.rename(source, join(targetPath, relativePath));
-  return relativePath;
+  let fileName = basename(source);
+  const relativePath = join(system, id);
+  if (isZip(source)) {
+    await fs.promises.rename(source, join(targetPath, relativePath, fileName));
+  } else {
+    console.log(`compressing ${fileName}`);
+    fileName = replaceExtension(fileName, 'zip');
+    await compressFile(source, join(targetPath, relativePath, fileName));
+  }
+  return join(relativePath, fileName);
 }
--- src/tools/file.ts
+++ src/tools/file.ts
@@ -4,3 +4,8 @@ export async function getFileSize(path: string) {
   const stats = await stat(path);
   return stats.size;
 }
+
+
+export function replaceExtension(path: string, extension: string): string {
+  return path.replace(/\.[^.]+$/, `.${extension}`);
+}
