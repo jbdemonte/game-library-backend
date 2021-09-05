5af4b4412d4e47c6af0a0b88a141b0d0ce5c982b
Jean-Baptiste DEMONTE <jbdemonte@gmail.com>
2021-09-05 17:08:59 +0200

    manage files

--- src/dropzone/index.ts
+++ src/dropzone/index.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import { basename, join } from 'path';
 import { FileWatcher } from '../tools/file-watcher';
 import { isZip } from '../tools/zip';
 import { IFileHash } from './lib/file-hash.interface';
@@ -27,15 +29,28 @@ async function onFile(path: string) {
 
 
 async function handleFile(fileHashes: IFileHash, size: number) {
-  const system = getRomSystemId(fileHashes);
-  if (!system) {
-    throw new Error(`System not found for file ${fileHashes.path}`);
-  }
   let rom = await findRom(fileHashes);
   if (rom) {
-    // already exists
+    console.log(`already existing, deleting ${basename(fileHashes.path)}`);
+    await fs.promises.unlink(fileHashes.path);
   } else {
+    const system = getRomSystemId(fileHashes);
+    if (!system) {
+      throw new Error(`System not found for file ${fileHashes.path}`);
+    }
+    console.log(`new rom for ${system} : ${basename(fileHashes.path)}`);
     rom = new romModel({...fileHashes, size, system});
+    rom.path = await moveFile(fileHashes.path, process.env.ROMS_PATH || '', system, rom.id);
     await rom.save();
   }
+}
+
+async function moveFile(source: string, targetPath: string, system: string, id: string): Promise<string> {
+  if (!targetPath) {
+    throw new Error('target path is missing');
+  }
+  await fs.promises.mkdir(join(targetPath, system, id), { recursive: true });
+  const relativePath = join(system, id, basename(source));
+  await fs.promises.rename(source, join(targetPath, relativePath));
+  return relativePath;
 }
