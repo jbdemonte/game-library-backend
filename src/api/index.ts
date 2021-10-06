3e91dcb28ed188b1be1cca4f1aa63174bca41ada
Jean-Baptiste DEMONTE <jbdemonte@gmail.com>
2021-10-06 17:46:19 +0200

    add cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { router as systems } from './systems';
 import { getServerConfig } from '../config';
 
 const config = getServerConfig();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors());
+}
 
 app.use('/systems', systems);
 
